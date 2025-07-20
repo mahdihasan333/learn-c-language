0bc2bcdc943901beb19a7dfb8a8395925bac7bcf
Mahdi Hassan <mahdioffi333@gmail.com>
2025-07-20 16:38:57 +0600

    first program

---
+++ basic/variables-data_types-input-output/hello.c
@@ -0,0 +1,53 @@
+#include <stdio.h>
+
+int main()
+{
+    // int age = 22;
+    // printf("age is %d", age);
+    
+    // float pi = 3.14;
+    //printf("age is %f \n", pi);
+    
+    // char star = '*';
+    // printf("star os %c \n", star);
+    
+    
+    //input number
+    //int age;
+    // printf("Enter Age");
+    // scanf("%d", &age);
+    // printf("age is : %d", age);
+    
+    
+    // input and sum
+    // int a, b;
+    // printf("enter a");
+    // scanf("%d", &a);
+    
+    // printf("enter b");
+    // scanf("%d", &b);
+    
+    // int sum = a + b;
+    // printf("sum is :" "%d", sum);
+    // printf("sum is : %d", a + b);
+    
+    
+    
+    // Write a program to calculate area of a square. (side is given)
+    // int side;
+    // printf("enter the side");
+    // scanf("%d", &side);
+    
+    // printf("area is : %d", side * side);
+    
+    
+    
+    // Write a program to calculate area of a circle. (side is given)
+    float radius;
+    printf("enter radius : ");
+    scanf("%f", &radius);
+    
+    printf("area is : %f", 3.14 * radius * radius);
+    
+     return 0;
+}
