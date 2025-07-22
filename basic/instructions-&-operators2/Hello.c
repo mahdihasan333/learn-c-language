208575d15efeadb0226f560e23dcb9d887c10127
Mahdi Hassan <mahdioffi333@gmail.com>
2025-07-22 16:58:14 +0600

    practice

--- basic/instructions-&-operators2/Hello.c
+++ basic/instructions-&-operators2/Hello.c
@@ -3,30 +3,82 @@
 
 int main()
 {
-    int a = 22;
-    int b = a;
-    int c = b * c;
-    int d = 1, e;
-
-    int oldAge = 22;
-    int years = 2;
-    int newAge = oldAge + years;
-
-    int x,y,z = 4;
-    x = y = z = 4;
-
-    int a = 1, b = 2;
-    int sum = a + b;
-    int multiply = a * b;
-    int x, y = a * b;
-
-    int b, c;
-    b = c = 1;
-    int a = b + c;
-    int power = pow(b, c);
-    printf("%d", power);
-
-    printf("%d", 16 % 10);
-
+    // Type Declaration Instructions
+    // int a = 22;
+    // int b = a;
+    // int c = b * c;
+    // int d = 1, e;
+    
+    // int oldAge = 22;
+    // int years = 2;
+    // int newAge = oldAge + years;
+    
+    // int x,y,z = 4;
+    // x = y = z = 4;
+    
+    
+    
+    
+    // int a = 1, b = 2;
+    // int sum = a + b;
+    // int multiply = a * b;
+    // int x, y = a * b;
+    
+    
+    
+    // int b, c;
+    // b = c = 1;
+    // int a = b + c;
+    // int power = pow(b, c);
+    // printf("%d", power);
+    
+    // printf("%d", 16 % 10);
+    
+    // printf("%d \n", -8 % 3);
+    // printf("%d \n", 8 % 3);
+    
+    
+    
+    // Arithmetic Instructions
+    // printf("%d \n", 2 * 2);
+    // printf("%f \n", 2.0 * 2);
+    // printf("%f \n", 2.0 * 2.0);
+    // printf("%f \n", 4 / 2.0);
+    
+    // printf("%d \n", 2 / 3);
+    // printf("%d \n", 3 / 2);
+    // printf("%f \n", 3.0 / 2);
+    
+    
+    //Practice Question 3
+    //Solve: int a = 1.999999; float number into integer
+    // int a = 1.999999;
+    // printf("%d \n", a);
+    
+    // int a = (int) 1.999999;
+    // printf("%d \n", a);
+    
+    
+    // Operator Precedence
+    // *, /, %
+    // +, -
+    // =
+    int a = 4 + 9 * 10;
+    // printf("%d \n", a);     // answer 9 * 10 + 4 = 94
+    
+    // Associativity (for same precedence) ---> Left to Right
+    // x = 4 * 3 / 6 * 2        //answer (4 * 3) /6) * 2 = 4
+    
+    int x = 4 * 3 / 6 * 2;
+    // printf("%d \n", x);
+    
+    
+    
+    // Practice Questions 4
+    // a. 5 * 2 - 2 * 3
+    // b. 5 * 2 / 2 * 3
+    // c. 5 * (2 / 2) * 3
+    // d. 5 + 2 / 2 * 3
+    
     return 0;
 }
