e298c8fa9cac4494866d0884c45ec90872a721c5
Mahdi Hassan <mahdioffi333@gmail.com>
2025-07-27 07:14:17 +0600

    added loop control statement

--- basic/loop-control-statements4/Hello.c
+++ basic/loop-control-statements4/Hello.c
@@ -170,18 +170,69 @@ int main()
     
     // Practice Question 18 ===> Keep taking numbers as input from user until user enters a number which is multiple of 7.
     
-    int n;
-    do {
-        printf("enter number: ");
-        scanf("%d", &n);
-        printf("%d \n", n);
+    // int n;
+    // do {
+    //     printf("enter number: ");
+    //     scanf("%d", &n);
+    //     printf("%d \n", n);
 
-        if(n %  == 0){
-            break;
-        }
-    } while(1);
-    printf("thank you!");
+    //     if(n %  == 0){
+    //         break;
+    //     }
+    // } while(1);
+    // printf("thank you!");
     
+    // continue Statement ===> skip to next iteration
+    // for(int i = 1; i <= 5; i++){
+    //     if(i == 3){
+    //         continue;
+    //     }
+    //     printf("%d \n", i);
+    // }
+    
+    // Practice Question 19 ===> Print all numbers form 1 to 10 excepts for 6.
+    // for(int i = 1; i <= 10; i++){
+    //     if(i == 6){
+    //         continue;
+    //     }
+    //     printf("%d \n", i);
+    // }
+    
+    // Practice Question 20 ===> Print all the odd numbers from 5 to 50.
+    // for(int i = 5; i <= 50; i++){
+    //     if(i % 2 != 0){
+    //         printf("%d \n", i);
+    //     }
+    // }
+    
+    // Practice Question 21 ===> Print the factorial of a number n.
+    // int n;
+    // printf("enter number: ");
+    // scanf("%d", &n);
+    
+    // int fact = 1;
+    // for(int i = 1; i <= n; i++){
+    //     fact = fact * i;
+    // }
+    // printf("final factorial is %d", fact);
+    
+    
+    // Practice Question 22 ===> Print reverse of the table for a number n.
+    // int n;
+    // printf("enter number: ");
+    // scanf("%d", &n);
+    
+    // for(int i = 10; i >= 1; i--){
+    //     printf("%d \n", n * i);
+    // }
+    
+    
+    // Practice Question 23 ===> calculate the sum of all numbers between 5 and 50. (including 5 & 50)
+    int sum = 0;
+    for(int i = 5; i <= 50; i++){
+        sum += i;
+    }
+    printf("sum is %d", sum);
     
     return 0;
         
