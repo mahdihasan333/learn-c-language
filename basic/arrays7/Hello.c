084bf238aad11b31d5cd7c301d1c1f96adb43b4a
Mahdi Hassan <mahdioffi333@gmail.com>
2025-07-30 05:55:17 +0600

    practice c array

---
+++ basic/arrays7/Hello.c
@@ -0,0 +1,99 @@
+// Online C compiler to run C program online
+#include <stdio.h>
+
+int main() {
+    // Array ===> Collection of similar data types stored at contiguous memory locations.
+    // int marks1 = 97;
+    // int marks2 = 98;
+    // int marks3 = 89;
+    
+    // int marks[] = {97, 98, 89};
+    
+    // Syntax
+    // int marks[3];
+    // char name[10];
+    // float price[2];
+    
+    // input & output
+    // scanf("%d", &marks[0]);
+    
+    // printf("%d", marks[0]);
+    
+    // int marks[3];
+    // printf("enter phy : ");
+    // scanf("%d", &marks[0]);
+    
+    // printf("enter chem : ");
+    // scanf("%d", &marks[1]);
+    
+    // printf("enter math : ");
+    // scanf("%d", &marks[2]);
+    
+    // printf("phy = %d, chem = %d, math = %d", marks[0], marks[1], marks[2]); 
+    
+    // Practice Question 42 ===> Write a program to enter price o f3 items & print their final cost with gst.
+    
+    // float price[3];
+    
+    // printf("enter 3 prices : ");
+    // scanf("%f", &price[0]);
+    // scanf("%f", &price[1]);
+    // scanf("%f", &price[2]);
+    
+    // printf("total price 1 : %f \n", price[0] + (0.18*price[0]));
+    // printf("total price 2 : %f \n", price[0] + (0.18*price[1]));
+    // printf("total price 3 : %f \n", price[0] + (0.18*price[2]));
+    
+    
+    // inititalization of array
+    // int marks[] = {97, 98, 89};
+    // int marks[3] = {97, 98, 89};
+    
+    // float price[] = {100.0, 200.0, 300.0};
+    
+    // Pointer Arithmetic ===> Pointer can be incremented & decremented
+    
+    // case - 1
+    // int age = 22;
+    // int *ptr = &age;
+    // printf("ptr = %u \n", ptr);
+    // ptr++;
+    // printf("ptr = %u \n", ptr);
+    
+    // case - 2
+    // float price = 20.00;
+    // float *ptr = &price;
+    // printf("ptr = %u \n", ptr);
+    // ptr++;
+    // printf("ptr = %u \n", ptr);
+    // ptr--;
+    // printf("ptr = %u \n", ptr);
+    
+    // case - 3
+    // char star = '*';
+    // char *ptr = &star;
+    // printf("ptr = %u \n", ptr);
+    // ptr++;
+    // printf("ptr = %u \n", ptr);
+    // ptr--;
+    // printf("ptr = %u \n", ptr);
+    
+    // Pointer Arithmetic
+    // -- We can also subtract one pointer from another.
+    // -- We can also compare 2 pointers.
+    
+    // int age = 22;
+    // int _age = 23;
+    // int *ptr = &age;
+    // int *_ptr = &_age;
+    // printf("%u, %u difference = %u \n", ptr, _ptr, ptr-_ptr);
+    // _ptr = &age;
+    // printf("comparison = %u \n", ptr == _ptr);
+    
+    // Array is a Pointer
+    // int *ptr = &arr[0];
+    // int *ptr = arr;
+    
+
+    return 0;
+}
