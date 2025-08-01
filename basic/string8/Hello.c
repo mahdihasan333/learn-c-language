12c0ead9bd5989330ec7999e56d826a5809461ab
Mahdi Hassan <mahdioffi333@gmail.com>
2025-08-01 20:53:27 +0600

    practice string

--- basic/string8/Hello.c
+++ basic/string8/Hello.c
@@ -1,5 +1,6 @@
 // Online C compiler to run C program online
 #include <stdio.h>
+#include <string.h>
 
 // Strings ==> A character array terminated by a '\O'(null character)
 // null character denotes string termination
@@ -92,15 +93,113 @@
 // }
 
 
-// Practice Question ===> Make a program that inputs user's name & prints its length.
+// Practice Question 49 ===> Make a program that inputs user's name & prints its length.
+
+// int countLength(char arr[]);
+// int main(){
+//     char name[100];
+//     fgets(name, 100, stdin);
+//     printf("length is : %d", countLength(name));
+//     return 0;
+// }
+
+// int countLength(char arr[]){
+//     int count = 0;
+//     for(int i = 0; arr[i] != '\0'; i++){
+//         count++;
+//     }
+//     return count;
+// }
 
 
+// Standard Library Functions <string.h>
+// 1 strlen(str) ===> count number of characters exculuding '\0'
+
+// int countLength(char arr[]);
+// int main(){
+//     char name[] = "Mahdi";
+//     int length = strlen(name);
+//     printf("length is : %d", length);
+//     return 0;
+// }
+
+// int countLength(char arr[]){
+//     int count = 0;
+//     for(int i = 0; arr[i] != '\0'; i++){
+//         count++;
+//     }
+//     return count;
+// }
 
 
+// Standard Library Funcitons <string.h>
+// 2 strcpy(newStr, oldStr) ===> copies value of old string to new string
+// int main(){
+//     char oldStr[] = "oldStr";
+//     char newStr[] = "newStr";
+//     strcpy(newStr, oldStr);
+//     puts(newStr);
+// }
 
 
+// Standard Library Funcitons <string.h>
+// 3 strcat(firstStr, secStr) ===> concatenates first string with second string
+
+// int main(){
+//     char firstStr[100] = "mahdi ";
+//     char secString[] = "Hassan";
+//     strcat(firstStr, secString);
+//     puts(firstStr);
+// }
 
 
+// Standard Library Funcitons <string.h>
+// 4 strcmp(firstStr, secStr) ===> Compares 2 strings & returns a value
+// 0 -> string equal
+// positive -> first > second(ASCII)
+// negative -> first < second (ASCII)
+
+// int main(){
+//     char firstStr[] = "Apple";
+//     char secStr[] = "Banana";
+//     printf("%d \n", strcmp(firstStr, secStr));
+// }
+
+// Practice Question 50 ===> Take a string input form the user using %c.
+
+// int main(){
+//     char str[100];
+//     char ch;
+//     int i = 0;
+    
+//     while(ch != '\n'){
+//         scanf("%c", &ch);
+//         str[i] = ch;
+//         i++;
+//     }
+//     str[i] = '\0';
+//     puts(str);
+// }
+
+
+// Practice Question 51 ===> Salting ---> Find the salting form of a password entered by user if the salt is "123" & added at the end.
+
+void salting(char password[]);
+
+int main(){
+    char password[100];
+    scanf("%s", password);
+
+}
+
+void salting(char password[]){
+    char salt[] = "123";
+    char newPass[200];
+    
+    strcpy(newPass, password);          // newPass = 'test';
+    strcpy(newPass, salt);              // newPass = "test"
+    puts(newPass);
+}
 
 
 
